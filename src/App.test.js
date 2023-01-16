d6ef1e0220d2c02d5a99d781f0b41da42f07ae04
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2023-01-16 16:47:27 +0700

    update App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const root = createRoot(document.getElementById('root'));
-  root.render(<App />, div);
+  ReactDOM.render(<App />, div);
 });
